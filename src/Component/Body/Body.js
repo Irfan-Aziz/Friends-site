a30564f5bd700e91284fd325779b888ab1904439
irfan <iaziz3900@gmail.com>
2020-04-17 06:30:07 +0600

    vezal

--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -7,11 +7,28 @@ import Connect from '../Connect/Connect';
 const Body = () => {
     const [friends,setFriends]=useState(data);
     const [connect,setConnect]=useState([]);
-    console.log(data);
+    //console.log(data);
     const handleAddFriend=(friend)=>{
-        console.log("Clicked",friend);
+        console.log("Clicked");
+        console.log(friend);
         const newConnection=[...connect,friend]; 
         setConnect(newConnection); 
+        //console.log(newConnection)
+        const add=document.getElementById("addBtn");
+        add.style.display="none";
+        const remove=document.getElementById("removeBtn");
+        remove.style.display="block";
+    }
+    const handleRemoveFriend=(friend)=>{
+        //console.log("Clicked remove");
+        const currentFriend=connect.filter((e,val)=>{if(e!==friend.val){
+        return e;}});
+        setConnect(connect);
+        console.log(currentFriend);
+        const add=document.getElementById("addBtn");
+        add.style.display="block";
+        const remove=document.getElementById("removeBtn");
+        remove.style.display="none";
     }
         
         return (
@@ -20,6 +37,7 @@ const Body = () => {
                {
                     friends.map(friend=><Celebrity
                         handleAddFriend={handleAddFriend}
+                        handleRemoveFriend={handleRemoveFriend}
                      friend={friend} ></Celebrity>)
                }    
                </div>
--- src/Component/Celebrty/Celebrity.js
+++ src/Component/Celebrty/Celebrity.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Celebrity.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Celebrity=(props)=>{
     const {name,email,phone,salary,img}=props.friend;
-    console.log({img});
+    //console.log({img});
 
     return (
         
@@ -18,12 +18,15 @@ const Celebrity=(props)=>{
                 <h4>E-mail: {email}</h4>
                 <h4>Cellphone no: {phone}</h4>
                 <h4>Salary: ${salary}</h4>
-                <button className="main-btn"
-          onClick={()=>props.handleAddFriend(props.friend)}><FontAwesomeIcon icon={faPlusCircle } /> Add Friend</button>
-
+               
+                <button className="main-btn" id="addBtn"
+          onClick={()=>props.handleAddFriend(email)}><FontAwesomeIcon icon={faPlusCircle } /> Add Friend</button>
+                <button className="main-btn remove" id="removeBtn"
+                onClick={()=>props.handleRemoveFriend(props.friend)}><FontAwesomeIcon icon={faMinusCircle}/>Remove Friend</button>
+                </div>
             </div>
             
-        </div>
+       
     );
     };
 
